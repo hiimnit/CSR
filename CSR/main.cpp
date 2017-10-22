85aff461a0a02e786cf4a80ad8bc8734abf66a4e
Filip Soukup <fsoukup@artex-is.cz>
2017-10-22 11:14:19 +0200

    Initial Commit

---
+++ CSR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSR
+//
+//  Created by Filip Soukup on 22/10/2017.
+//  Copyright © 2017 Filip Soukup. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
